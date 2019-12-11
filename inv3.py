c9a003aaddeec357db7c29bc29fea9b10e4dbc7c
ksiezna <48408914+ksiezna@users.noreply.github.com>
2019-12-11 12:00:57 +0100

    Update inv3.py

--- inv3.py
+++ inv3.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import json
 
 d = {"grupa1": {"hosts": ['server30.example.com', 'server.example.com'],"vars": {"trabant": "jest"}},"_meta": {"hostvars": {"server30.example.com": {"trabant": "nie ma","auto": " o to to"}}}}
