761949d6d11621f989086ed1fa59be43b141d12d
root <root@server30.example.com>
2019-12-11 13:05:33 +0100

    any-chg

--- inv3.py
+++ inv3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import json
 
-d = {"grupa1": {"hosts": ['server30.example.com', 'server.example.com'],"vars": {"trabant": "jest"}},"_meta": {"hostvars": {"server30.example.com": {"trabant": "nie ma","auto": " o to to","remote_port":22,"remote_user":"mat","become_method":"sudo"}}}}
+d = {"grupa1": {"hosts": ['server30.example.com', 'server.example.com'],"vars": {"trabant": "jest"}},"_meta": {"hostvars": {"server30.example.com": {"trabant": "nie ma","auto": " o to to","ansible_port":22,"ansible_user":"mat","become_method":"sudo"}}}}
 
 print(json.dumps(d))
